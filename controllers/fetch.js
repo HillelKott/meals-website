4264be778a88885ea7e57ac31e4859e084a4794a
Glitch (same-verdict-uflqct4eld) <none>
2020-02-09 19:37:04 +0000

    🏄🚇 Checkpoint ./controllers/fetch.js:8814991/137 ./models/app2.txt:8814991/36

--- controllers/fetch.js
+++ controllers/fetch.js
@@ -1,16 +1,10 @@
 export const getRecepi = (id, recipe) => {
 
     if (id) {
-        console.log(id);
-
         return fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
             .then(data => data.json())
-            // .then(function (data) {
-            //         console.log(data)
-            //     })
-                    .catch(e => console.log(e))
+            .catch(e => console.log(e))
     } else {
-
         return fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe}`)
             .then(data => data.json())
             .catch(e => console.log(e))
